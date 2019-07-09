3f2f53032e1a01e46639a05d2af4b4e850e8ca85
d-tsuji <dram.dt.shonan@gmail.com>
2019-07-09 23:36:37 +0900

    modify

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,8 @@ author = 'tsuji'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo'
-]
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.githubpages'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
